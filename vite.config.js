d6cca1ce7ac6ac286e3109d65d3769b2e48ca7fe
catboss1995 <102404002@ydu.edu.tw>
2025-09-11 09:41:58 +0800

    up

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react' // 如果是 React
 
 export default defineConfig({
   plugins: [react()],
-  base: '/resume-portfolio/', // ← 重要！你的 repo 名稱
+  base: './', 
   build: {
     outDir: 'dist'
   }
